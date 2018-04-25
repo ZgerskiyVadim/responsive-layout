bc887c6a8bb1ee85dfc83c4e9f1de50081b101c7
oxy <zgerskiy.vadim@mail.ru>
2018-04-25 17:10:14 +0300

    Responsive design

---
+++ js/index.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', function () {
+    const toggleMenu = document.querySelector('.header__toggle-menu');
+    const showToggle = document.querySelectorAll('.nav-toggle')[0];
+    const hideToggle = document.querySelectorAll('.nav-toggle')[1];
+    const containerCover = document.querySelector('.container-cover');
+
+    showToggle.addEventListener('click', function () {
+        this.classList.add('hide');
+        toggleMenu.classList.add('show-toggle');
+        containerCover.classList.add('cover-bg');
+        hideToggle.classList.remove('hide');
+    });
+
+    hideToggle.addEventListener('click', function () {
+        hideToggleMenu();
+    });
+
+    containerCover.addEventListener('click', function () {
+        hideToggleMenu();
+    });
+
+    function hideToggleMenu() {
+        hideToggle.classList.add('hide');
+        showToggle.classList.remove('hide');
+        toggleMenu.classList.remove('show-toggle');
+        containerCover.classList.remove('cover-bg');
+    }
+});
